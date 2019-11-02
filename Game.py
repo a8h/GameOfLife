c4c852b6bd96a0eeb9cbdad42488bd13faaafaff
Arun <aa524@cornell.edu>
2019-11-02 19:56:22 +0100

    Some Pylint fixes

--- Game.py
+++ Game.py
@@ -7,19 +7,8 @@ import locale
 import logging
 
 """
-The universe of the Game of Life is an infinite, two-dimensional orthogonal grid of square cells, each of which is in one of two possible states, alive or dead, (or populated and unpopulated, respectively). 
-Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent.
-At each step in time, the following transitions occur:
-
-    Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-    Any live cell with two or three live neighbours lives on to the next generation.
-    Any live cell with more than three live neighbours dies, as if by overpopulation.
-    Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-    The initial pattern constitutes the seed of the system.
-    The first generation is created by applying the above rules simultaneously to every cell in the seed; births and deaths occur simultaneously, and the discrete moment at which this happens is sometimes called a tick. 
-    Each generation is a pure function of the preceding one.
-    The rules continue to be applied repeatedly to create further generations.
-
+Conway's Game of Life:
+https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Rules
 """
 
 locale.setlocale(locale.LC_ALL, '')
@@ -29,16 +18,19 @@ try:
 except NameError:
     xrange = range
 
-def rand_init_grid(num_rows, num_cols, with_border = False):
+def rand_init_grid(num_rows, num_cols, with_border=False):
     # A grid with 0s on the outsides and random 0s or 1s in the inside
     num_rows, num_cols = int(num_rows), int(num_cols)
     if with_border:
-        return [[random.randint(0,1) if i > 0 and i < num_cols - 1 else 0 for i in xrange(num_cols)] if i > 0 and i < num_rows - 1 else [0] * num_cols for i in xrange(num_rows)]
+        return [[random.randint(0,1) if i > 0 and i < num_cols - 1 else 0 \
+            for i in xrange(num_cols)] if i > 0 and i < num_rows - 1 \
+            else [0] * num_cols for i in xrange(num_rows)]
     else:
         return [[random.randint(0,1) for i in xrange(num_cols)] for i in xrange(num_rows)]
     
-def printGrid(grid, symbol_live = u'\u2584', symbol_dead = ' '):
-    return b'\n'.join([b' '.join([symbol_live.encode('UTF-8') if i else symbol_dead.encode('UTF-8') for i in list]) for list in grid])
+def printGrid(grid, symbol_live=u'\u2584', symbol_dead=' '):
+    return b'\n'.join([b' '.join([symbol_live.encode('UTF-8') if i \
+            else symbol_dead.encode('UTF-8') for i in list]) for list in grid])
 
 #Assuming grids are rectangular
 def state_transition(current_grid, future_grid):
